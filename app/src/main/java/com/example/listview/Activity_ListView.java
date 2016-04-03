8269f61088e45ce70165ca9cd0f6e62f9072d556
David <Dgbaker93@gmail.com>
2016-04-03 16:53:37 -0400

    fixed toolbar error

--- app/src/main/java/com/example/listview/Activity_ListView.java
+++ app/src/main/java/com/example/listview/Activity_ListView.java
@@ -3,11 +3,11 @@ package com.example.listview;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toolbar;
 
 public class Activity_ListView extends AppCompatActivity {
 
